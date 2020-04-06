39c402b6cc81b6de84a66f6638db1e75c4e06805
Tshepo <rocboyt@gmail.com>
2020-04-06 18:40:45 +0200

    Added more controls

---
+++ Dice game/colors/LuckyNumberBannerColor.swift
@@ -0,0 +1,13 @@
+//
+//  LuckyNumberBannerColor.swift
+//  Dice game
+//
+//  Created by Tshepo Mahlaula on 2020/04/06.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class LuckyNumberBannerColor: UIColor {
+
+}
